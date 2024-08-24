86f94188000ce3d3f299e7ebfa26cb42f4018852
dhaneshkaushan <dhaneshkaushan@gmail.com>
2024-08-25 00:05:02 +0530

    getting data from database

--- src/main/java/com/dhanesh/studentsystem/controller/StudentController.java
+++ src/main/java/com/dhanesh/studentsystem/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.dhanesh.studentsystem.controller;
 import com.dhanesh.studentsystem.model.Student;
 import com.dhanesh.studentsystem.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,5 +18,10 @@ public class StudentController {
     public String add(@RequestBody Student student){
         studentService.saveStudent(student);
         return "New Student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
     }
 }
--- src/main/java/com/dhanesh/studentsystem/service/StudentService.java
+++ src/main/java/com/dhanesh/studentsystem/service/StudentService.java
@@ -2,6 +2,8 @@ package com.dhanesh.studentsystem.service;
 
 import com.dhanesh.studentsystem.model.Student;
 
+import java.util.List;
 public interface StudentService {
     public Student saveStudent(Student student);
+    public List<Student> getAllStudents();
 }
